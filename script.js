b33d8a983f6614ca0e6fae8acd1e550c2bc19769
JVON_CLEMMER CLEMMER_JVON <jclemmer90@gmail.com>
2022-12-12 22:35:19 -0600

    next commit

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
+var textAreaEl = document.querySelector('#textarea');
+var typefaceEl = document.querySelector('#typeface');
+
+var 
+
+
+var typeface;
+var element = [alphaNumericCharacters]
+
 
 // Write password to the #password input
-function writePassword() {
+typefaceEl.addEventListener('change', function (event){
+    event.preventDefault();
+    typeface = typefaceEl.value;
+    document.querySelector('.container').style.fontfamily = typeface;
+});
+
+function writePassword(password) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  console.log(password.key);
+
+textAreaEl.addEventListener("generate")
+var alphaNumericCharacters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvzxyz1234567890".split(
+);
+if (alphaNumericCharacters.includes(password)){
+    for (var i =0; i elements.length; i++) {
+
+        elements[i].textContent += password.key
+    }
+}
 
 }
 
